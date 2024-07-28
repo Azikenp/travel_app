8168013d8586de54853d80f77b62c56819ba383c
azikenp <azikenp26@gmail.com>
2024-07-28 09:08:54 +0100

    continuation on features

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { FEATURES } from "@/constants";
+import Image from "next/image";
 
 const Feature = () => {
   return (
-    <div>Feature</div>
-  )
-}
+    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        {/* left */}
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
 
-export default Feature
+        {/* right */}
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              className="absolute left-[-5px] top-[-28px] w-10 lg:1-[50px]"
+              width={50}
+              height={50}
+              alt="camp"
+              src="/camp.svg"
+            />
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+          </div>
+          <ul>
+            {FEATURES.map((feature) => {
+              return (
+                <FeatureItem
+                  title={feature.title}
+                  key={feature.title}
+                  icon={feature.icon}
+                  description={feature.description}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+type featureItem = {
+  title: string;
+  icon: string;
+  description: string;
+};
+
+const FeatureItem = ({ title, icon, description }: featureItem) => {
+  return (
+    <li className="flex w-full flex-col flex-1 items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none" >{description}</p>
+    </li>
+  );
+};
+
+export default Feature;
